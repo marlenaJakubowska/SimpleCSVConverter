35d65213db13605e7af792119602f251f6550260
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-11-22 16:22:09 +0100

    Add structure

---
+++ src/test/java/com/codecool/scc/SccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.scc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SccApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
